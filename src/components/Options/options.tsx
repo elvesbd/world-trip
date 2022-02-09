50a265431cf764be8aa5b21bfc7e199e6f40f71d
elvesbd <elvesbd@yahoo.com>
2022-02-08 21:31:17 -0300

    add alt text

--- src/components/Options/options.tsx
+++ src/components/Options/options.tsx
@@ -13,7 +13,7 @@ export function Option({icon, text}:OptionProps ) {
 
   return (
     <Flex direction={["row","column"]} align="center" justify="center">
-      {isMobile ? <Image src={`/${icon}.png`} w="85px" h="85px" mb="6" /> : <Text color="yellow.400" fontSize="4xl" mr="2">•</Text>}
+      {isMobile ? <Image src={`/${icon}.png`} w="85px" h="85px" mb="6" alt='Opções'/> : <Text color="yellow.400" fontSize="4xl" mr="2">•</Text>}
       <Text fontWeight="600" color="gray.700" fontSize={["md", "xl","2xl"]}>{text}</Text>
     </Flex>
   )
