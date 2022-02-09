994a06d0b667be737ced3175a57d4a806462ae98
elvesbd <elvesbd@yahoo.com>
2022-02-08 21:11:59 -0300

    create component banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,20 @@
+import React from "react";
 import Prismic from '@prismicio/client';
 import { Box, Image, Flex, Text, Heading } from "@chakra-ui/react";
-import React from "react";
 import { Header } from "../components/Header";
 import { Options } from "../components/Options";
 import { Separator } from "../components/Separator";
 import { Slider } from "../components/Slider";
 import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../services/prismic';
+import { Banner } from "../components/banner";
 
 
 export default function Home({ continents }) {
   return (
-   <>
+   <Flex direction='column'>
       <Header />
-      <Box height={335}>
-        <Image position='absolute' src='/Background.png' alt='Airplane' />
-        <Flex marginX='32' position='relative' align='center' justify='space-between'>
-          <Box>
-            <Flex direction='column' width='500'>
-              <Text 
-                width={426} 
-                height={108} 
-                fontSize={34} 
-                color='light.info.100' 
-                fontWeight='bold' 
-              >
-                5 Continentes, <br/>infinitas possibilidades.
-              </Text>
-              <Text 
-                width={521} 
-                height={57} 
-                mt='2' 
-                mb='6' 
-                fontSize={20} 
-                color='light.info.300' 
-                fontWeight='400'
-              >
-                Chegou a hora de tirar do papel a viagem que você sempre sonhou. 
-              </Text>
-            </Flex>
-          </Box>
-          <Box mt='16'>
-            <Image src='/Airplane.png' alt='Airplane'/>
-          </Box>
-        </Flex>
-      </Box>
+      <Banner />
       <Options />
       <Separator />
 
@@ -60,7 +30,7 @@ export default function Home({ continents }) {
       </Heading>
 
       <Slider continents={continents} />
-   </>  
+   </Flex>  
   );
 }
 
